5ea00e737b1aa99ee90b863a734b48812fb4f313
Kirubel <kirubeltekle9@gmail.com>
2025-01-29 21:19:05 -0500

    feat: full functionality add it

--- scrap.py
+++ scrap.py
@@ -4,59 +4,61 @@ from queue import Queue
 import threading
 import sys
 import os
-from datetime import datetime
 import shutil
 
 ROOT_DOWNLOADS_FOLDER_NAME="GetGit-Downloads/"
 BRANCH=None
 
-demo_repo_url = "https://github.com/bkimo/linear-algebra-python"
-targets = Queue()
+demo_repo_url = "https://github.com/pawelborkar/awesome-repos"
+root_targets = Queue()
+dir_targets = Queue()
 
-def scrap(url: str, branch: str=None, ext:str = "pdf"):
+def scrap(url: str, branch: str=None, ext:str = "pdf", from_dir: bool = False):
     global BRANCH
 
     # GET request to repository
     resp = requests.get(url)
 
     # BS instance from response and parsing with html.parser
-    soup = BS(resp.content, 'html.parser')
-    
+    soup = BS(resp.text, 'html.parser')
+
     # Assign branch name dynamically
     if branch is not None:
         BRANCH = branch
     else:
         BRANCH = soup.find('button', attrs={'id': 'branch-picker-repos-header-ref-selector'})
         BRANCH = BRANCH.findChildren('span', attrs={'class': 'eMMFM'})[0].text.strip()
-    
+
     # Traverse the repo
-    arrange(soup, ext)
+    arrange(soup, ext, from_dir)
     
-def arrange(soup: BS, ext):
+def arrange(soup: BS, ext: str, from_dir: bool = False):
     # Search for all 'a' that are found in the repo
     for a in soup.find_all('a', attrs={'class': f'Link--primary'}):
         # Check if it is downloadable and endswith the specified extension
-
         if "/blob/" in a['href']:
             # Test if it is a file
             if f"/blob/{BRANCH}/" in a['href'] and a['href'].endswith(f'.{ext}'):
                 target = {
                     'type': 'file',
-                    'url': a['href']
+                    'url': a['href'],
+                    'from_dir': from_dir
                 }
-                
-                # targets.put(target)
+                if from_dir:
+                    dir_targets.put(target)
+                else:
+                    root_targets.put(target)
                 
         elif "/tree/" in a['href']:
             # Test if it is a directory
+            url = f"https://github.com{a['href']}"
             target = {
                 'type': 'dir',
-                'url': demo_repo_url+ a['href']
+                'url': url,
             }
-            targets.put(target)
+            root_targets.put(target)
 
-def download_content(url: str, sess: requests.Session):
-    return
+def download_content(url: str, sess: requests.Session, dir_path: str = None):
     endpoint = "https://raw.githubusercontent.com" + url
     endpoint = endpoint.replace('/blob/', '/refs/heads/')
 
@@ -64,50 +66,56 @@ def download_content(url: str, sess: requests.Session):
     resp = sess.get(url=endpoint, stream=True)
     
     if resp.status_code == 200:
-        with open(ROOT_DOWNLOADS_FOLDER_NAME+fname, "wb") as f:
+        with open((dir_path or "") +fname, "wb") as f:
             for chunk in resp.iter_content(chunk_size=1024):
                 f.write(chunk)
-            print(f"[+] Downloaded: {fname}")
+            sys.stdout.write(f"[+] Downloaded: {fname}\n");sys.stdout.flush()
     else:
         sys.stdout.write("x");sys.stdout.flush()
     
+def download_content_from_dir(dir_url: str, branch: str, ext: str):
+    scrap(url=dir_url, branch=branch, ext=ext, from_dir=True)
 
-def download_content_from_dir(dir_url, sess: requests.Session):
-    dir_name = dir_url.split('/')[-1]
-
-    try:
-        os.mkdir(ROOT_DOWNLOADS_FOLDER_NAME+dir_name)
-    except FileExistsError as e:
-        shutil.rmtree(ROOT_DOWNLOADS_FOLDER_NAME+dir_name)
-        os.mkdir(ROOT_DOWNLOADS_FOLDER_NAME+dir_name)
-        
-    scrap(url=dir_url)
-
-def main():
-    scrap(demo_repo_url, ext="pdf")
+def main(ext: str, branch: str, repo_url:str):
+    scrap(repo_url, branch, ext=ext)
 
     session = requests.session()
     threads = []
 
-    if not targets.empty():
+    if not root_targets.empty():
         try:
             os.mkdir(ROOT_DOWNLOADS_FOLDER_NAME)
         except FileExistsError as e:
-            shutil.rmtree(ROOT_DOWNLOADS_FOLDER_NAME)
-            os.mkdir(ROOT_DOWNLOADS_FOLDER_NAME)
+            pass
 
-    while not targets.empty():
-        target = targets.get()
+    while not root_targets.empty():
+        root_target = root_targets.get()
 
-        if target['type'] == 'file':
-            content_thread = threading.Thread(target=download_content, args=(target['url'],session))
+        if root_target['type'] == 'file':
+            content_thread = threading.Thread(target=download_content, args=(root_target['url'],session, ROOT_DOWNLOADS_FOLDER_NAME))
             content_thread.start()
             threads.append(content_thread)
 
-        elif target['type'] == 'dir':
-            dir_thread = threading.Thread(target=download_content_from_dir, args=(target['url'],session))
+        elif root_target['type'] == 'dir':
+            dir_thread = threading.Thread(target=download_content_from_dir, args=(root_target['url'], branch, ext))
             dir_thread.start()
             threads.append(dir_thread)
+    
+    for thread in threads:
+        thread.join()
+
+    while not dir_targets.empty():
+        dir_target = dir_targets.get()
+        dir_path = ROOT_DOWNLOADS_FOLDER_NAME + dir_target['url'].split("/")[-2] + "/"
+
+        try:
+            os.mkdir(dir_path)
+        except FileExistsError as e:
+            pass
+
+        url = dir_target['url']
+
+        download_content(url, sess=session, dir_path=dir_path)
 
 if __name__ == '__main__':
     main()
